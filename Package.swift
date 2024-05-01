62e3824f5343f90350ea998799545e2268e2e437
Nikhil Nigade <nikhil@dezinezync.com>
2024-05-01 17:38:50 +0530

    update package and target versions
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Package.swift
+++ Package.swift
@@ -1,23 +1,20 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
-    name: "FeedKit",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .tvOS(.v12),
-        .watchOS(.v4)
-    ],
-    products: [
-        .library(name: "FeedKit", targets: ["FeedKit"]),
-    ],
-    targets: [
-        .target(name: "FeedKit", dependencies: []),
-        .testTarget(name: "Tests", dependencies: ["FeedKit"], path: "Tests")
-    ],
-    swiftLanguageVersions: [
-        .v5
-    ]
+  name: "FeedKit",
+  platforms: [
+    .macOS(.v11),
+    .iOS(.v16),
+    .tvOS(.v16),
+    .watchOS(.v8)
+  ],
+  products: [
+    .library(name: "FeedKit", targets: ["FeedKit"]),
+  ],
+  targets: [
+    .target(name: "FeedKit", dependencies: []),
+    .testTarget(name: "Tests", dependencies: ["FeedKit"], path: "Tests")
+  ]
 )
